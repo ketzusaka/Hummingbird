7675b91403930c9e7d389f456c1f8b37b35ea94a
ketzusaka <james@weebly.com>
2016-03-23 07:39:18 -0700

    Lock to more specific version of Strand

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
 	name: "Hummingbird",
     dependencies: [
-        .Package(url: "https://github.com/ketzusaka/Strand", majorVersion: 1)
+        .Package(url: "https://github.com/ketzusaka/Strand", majorVersion: 1, minor: 0)
     ]
 )
 
