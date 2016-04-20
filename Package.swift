1da3c55f26f5ff97d7d7bcc65d5de2639a7e24d1
ketzusaka <james@weebly.com>
2016-04-20 07:35:16 -0700

    Use canonical URL for Strand dependency

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
 	name: "Hummingbird",
     dependencies: [
-        .Package(url: "https://github.com/ketzusaka/Strand", majorVersion: 1, minor: 2),
+        .Package(url: "https://github.com/ketzusaka/Strand.git", majorVersion: 1, minor: 2),
         .Package(url: "https://github.com/open-swift/C7.git", majorVersion: 0, minor: 5)
     ]
 )
