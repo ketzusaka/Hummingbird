28a3068e7f03ebed1c07b948125cf632152bab5c
Tanner Nelson <tanner@bluebite.com>
2016-05-04 19:01:11 -0400

    snapshot 05-03

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
 	name: "Hummingbird",
     dependencies: [
         .Package(url: "https://github.com/ketzusaka/Strand.git", majorVersion: 1, minor: 2),
-        .Package(url: "https://github.com/open-swift/C7.git", majorVersion: 0, minor: 6)
+        .Package(url: "https://github.com/open-swift/C7.git", majorVersion: 0, minor: 7)
     ]
 )
 
