4f735be808c4f6e53030223d4008c24e50f3daab
Tanner Nelson <tanner@bluebite.com>
2016-05-24 11:47:19 -0400

    update to Open Swift 0.8

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
 	name: "Hummingbird",
     dependencies: [
         .Package(url: "https://github.com/ketzusaka/Strand.git", majorVersion: 1, minor: 3),
-        .Package(url: "https://github.com/open-swift/C7.git", majorVersion: 0, minor: 7)
+        .Package(url: "https://github.com/open-swift/C7.git", majorVersion: 0, minor: 8)
     ]
 )
 
